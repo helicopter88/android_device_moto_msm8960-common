CyanogenMod/android_device_htc_msm8960-common

msm8960-common: add surfaceflinger client stub to common repo
All HTC 8960 camera HALs currently depend on having this library.

Change-Id: I0b63e91e5f795ab674a6a33e9ad96adb48ec7c50

libsurfaceflinger_client/Android.mk
@@ -1,0 +1,14 @@
+ifeq ($(CAMERA_USES_SURFACEFLINGER_CLIENT_STUB),true)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES :=
+
+LOCAL_SHARED_LIBRARIES :=
+
+LOCAL_MODULES := libsurfaceflinger_client
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif

helicopter88/android_device_moto_msm8960-common

libsurfaceflinger_client/Android.mk
@@ -1,0 +1,14 @@
+ifeq ($(CAMERA_USES_SURFACEFLINGER_CLIENT_STUB),true)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES :=
+
+LOCAL_SHARED_LIBRARIES :=
+
+LOCAL_MODULE := libsurfaceflinger_client
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif